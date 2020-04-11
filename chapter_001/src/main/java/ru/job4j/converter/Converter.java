f30ee64b09bb42900a072a7640fae4be7217e68f
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-04-11 17:01:12 +0300

    5. Конвертер валюты[#270896]

---
+++ chapter_001/src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,24 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static void main(String[] args){
+        float euro = rubleToEuro(140);
+        float dollar = rubleToDollar(140);
+
+        System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("140 rubles are " + dollar + " dollar.");
+
+
+    }
+
+    public static float rubleToEuro(float value){
+        float rsl = value / 70;
+        return rsl;
+    }
+    public static float rubleToDollar(float value){
+        float rsl = value / 60;
+        return rsl;
+    }
+
+}
