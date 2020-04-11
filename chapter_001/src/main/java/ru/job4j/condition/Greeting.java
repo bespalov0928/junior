2a73bc00e10d21a39fc1bd9f5a754a3e64b401d2
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-04-11 22:39:43 +0300

    9.2. Checkstyle[#270911]

--- chapter_001/src/main/java/ru/job4j/condition/Greeting.java
+++ chapter_001/src/main/java/ru/job4j/condition/Greeting.java
@@ -1,14 +1,13 @@
 package ru.job4j.condition;
 
 public class Greeting {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String idea = "I like Java!";
         int year = getCurrentYear();
-        System.out.println(idea+" But I am a newbie."+year);
+        System.out.println(idea + " But I am a newbie." + year);
     }
 
-    public static int getCurrentYear()
-    {
+    public static int getCurrentYear() {
         java.util.Calendar calendar = java.util.Calendar.getInstance(java.util.TimeZone.getDefault(), java.util.Locale.getDefault());
         calendar.setTime(new java.util.Date());
         return calendar.get(java.util.Calendar.YEAR);
