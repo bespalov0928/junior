510b462af4be6f0a66c74b02a88453391ea27b8b
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-04-19 00:39:54 +0300

    1.1.5.6.7.1. Моно строка в матрице.[#270882]

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+/**
+ * class MatrixCheck проверrf, что строка в двухмерном массиве целиком заполнена символом 'X'
+ * @author bespalov
+ */
+public class MatrixCheck {
+    /**
+     *
+     * @param board массив
+     * @param row номер строки
+     * @return результат проверки
+     */
+    public static boolean monoHorizontal(char[][] board, int row) {
+        boolean result = true;
+        for (int index = 0; index < board.length; index++) {
+            char value = board[row][index];
+            if (value != 'X') {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
