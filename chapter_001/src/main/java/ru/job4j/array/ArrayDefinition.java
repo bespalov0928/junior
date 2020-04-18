cd60e4118995a0eae31531324e03846aaf16ee84
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-04-18 11:18:24 +0300

    1.1.5.6.0.2. Заполнение массива.[#270885]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,15 @@ public class ArrayDefinition {
         short[] x = new short[5];
         String[] y = new String[100500];
         float[] z = new float[40];
+
+        String[] names = new String[4];
+        names[0] = "Nikola";
+        names[1] = "Mihail";
+        names[2] = "Roman";
+        names[3] = "Artem";
+
+        for (int index = 0; index < names.length; index++) {
+            System.out.println(names[index]);
+        }
     }
 }
