7a5ddd22d8af8c7e59a4e0eea3f1ac30a4f57a0b
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-04-18 11:09:05 +0300

    1.1.5.6.0.1. Объявление массива.[#270893]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,9 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] x = new short[5];
+        String[] y = new String[100500];
+        float[] z = new float[40];
+    }
+}
