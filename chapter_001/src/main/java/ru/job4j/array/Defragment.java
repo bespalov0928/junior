b4dd8c60aa8ad2f80ca11259fc9ed6d1f345cc8b
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-04-19 14:58:40 +0300

    1.1.5.6.8. Дефрагментация массива.[#270892]

---
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,41 @@
+package ru.job4j.array;
+
+/**
+ * class Defragment перемещаtn заполненные ячейки массива в начало, а пустые в конец.
+ *
+ * @author bespalov
+ */
+public class Defragment {
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+
+    /**
+     * перемещеине заполненных ячеек масива в начало
+     *
+     * @param array массив
+     * @return обработанный масив
+     */
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int point = index;
+                while (point < array.length && array[point] == null) {
+                    point++;
+                }
+                if (point >= array.length) {
+                    break;
+                }
+                array[index] = array[point];
+                array[point] = null;
+            }
+            System.out.println(array[index] + " ");
+        }
+        return array;
+    }
+}
