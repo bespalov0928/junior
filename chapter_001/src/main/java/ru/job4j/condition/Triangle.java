e0d3614ce4804efb74e339e5c0089ef4a1386f35
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-04-26 12:45:20 +0300

    1.2.1.2.2. Рефакторинг - Площадь треугольника.[#271030]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -6,14 +6,67 @@ package ru.job4j.condition;
  */
 public class Triangle {
 
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
     /**
-     * сумма двух сторон должна быть бльше третьей
-     * @param ab сторона треугольника
-     * @param ac сторона треугольника
-     * @param bc сторона треугольника
+     * Метод вычисления периметра по длинам сторон.
+     * <p>
+     * Формула.
+     * <p>
+     * (a + b + c) / 2
+     *
+     * @param a расстояние между точками a b
+     * @param b расстояние между точками a c
+     * @param c расстояние между точками b c
+     * @return Периметр.
+     */
+    public double period(double a, double b, double c) {
+        return a + b + c;
+    }
+
+    /**
+     * Метод должен вычислить площадь треугольника.
+     *
+     * Формула.
+     *
+     * √ p *(p - ab) * (p - ac) * (p - bc)
+     *
+     * где √ - корень квадратный, для извлечения корня использовать метод Math.sqrt().
+     *
+     * @return Вернуть площадь, если треугольник существует или -1.
+     */
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        if (this.exit(a, b, c)) {
+            rsl = Math.sqrt((p - a) * (p - b) * (p - c));
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     *
+     * Подумайте какое надо написать условие, чтобы определить можно ли построить треугольник.
+     *
+     * @param ab Длина от точки a b.
+     * @param ac Длина от точки a c.
+     * @param bc Длина от точки b c.
      * @return
      */
     public static boolean exit(double ab, double ac, double bc) {
         return (ab + ac > bc) && (ac + bc > ab) && (bc + ab > ac);
     }
+
 }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -19,5 +20,38 @@ public class TriangleTest {
     public void whenExistFalse() {
         boolean result = Triangle.exit(2.0, 2.0, 5.0);
         assertThat(result, is(false));
+    }
+
+    @Test
+    public void periodTrue() {
+        Point first = new Point(5, 1);
+        Point second = new Point(1, 5);
+        Point third = new Point(5, 5);
+
+        double ab = 2;
+        double ac = 3;
+        double bc = 4;
+
+        double p;
+        double expected = 9;
+        Triangle trg = new Triangle(first, second, third);
+        p = trg.period(ab, ac, bc);
+        Assert.assertEquals(expected, p, 0.01);
+    }
+
+
+
+    @Test
+    public void area() {
+        Point first = new Point(5, 1);
+        Point second = new Point(4, 2);
+        Point third = new Point(5, 2);
+
+        double s;
+        double expected = 3.414;
+        Triangle trg = new Triangle(first, second, third);
+        s = trg.area();
+        Assert.assertEquals(expected, s, 0.01);
+
     }
 }
