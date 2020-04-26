794e928a27910c48683e4770f3a56eedf992ff5c
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-04-26 11:14:18 +0300

    1.2.1.2.1. Рефакторинг - Расстояние между точками.[#271029]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,37 +1,74 @@
 package ru.job4j.condition;
 
 
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
 /**
  * class Point строит линию
+ *
  * @author bespalov
  */
 public class Point {
 
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
     public static void main(String[] args) {
         double result;
-        result = distance(0, 0, 2, 0);
-        System.out.println("distance(0,0,2,0) " + result);
-        result = distance(0, 0, 3, 0);
-        System.out.println("distance(0, 0, 3, 0) " + result);
-        result = distance(0, 0, 4, 0);
-        System.out.println("distance(0, 0, 4, 0) " + result);
+
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 
     /**
-     * возвращет квадратный корень между точками
-     * @param x1
-     * @param y1
-     * @param x2
-     * @param y2
-     * @return
+     * вычисление растояния между точками
+     * @param that объект точка
+     * @return расстояние
      */
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl;
-        double x = x2 - x1;
-        double y = y2 - y1;
-        x = Math.pow(x, 2);
-        y = Math.pow(y, 2);
-        rsl = Math.sqrt(x + y);
-        return rsl;
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
+
+
+
+
+
+//    public static void main(String[] args) {
+//        double result;
+//        result = distance(0, 0, 2, 0);
+//        System.out.println("distance(0,0,2,0) " + result);
+//        result = distance(0, 0, 3, 0);
+//        System.out.println("distance(0, 0, 3, 0) " + result);
+//        result = distance(0, 0, 4, 0);
+//        System.out.println("distance(0, 0, 4, 0) " + result);
+//    }
+
+//    /**
+//     * возвращет квадратный корень между точками
+//     *
+//     * @param x1
+//     * @param y1
+//     * @param x2
+//     * @param y2
+//     * @return
+//     */
+//    public static double distance(int x1, int y1, int x2, int y2) {
+//        double rsl;
+//        double x = x2 - x1;
+//        double y = y2 - y1;
+//        x = Math.pow(x, 2);
+//        y = Math.pow(y, 2);
+//        rsl = Math.sqrt(x + y);
+//        return rsl;
+//    }
+
+
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -9,13 +9,23 @@ import static org.junit.Assert.*;
 
 public class PointTest {
     @Test
+
     public void distance() {
-        int x1 = 0;
-        int x2 = 0;
-        int x3 = 2;
-        int x4 = 0;
+
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
         double expected = 2;
-        double out = Point.distance(x1, x2, x3, x4);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
+
+//    public void distance() {
+//        int x1 = 0;
+//        int x2 = 0;
+//        int x3 = 2;
+//        int x4 = 0;
+//        double expected = 2;
+//        double out = Point.distance(x1, x2, x3, x4);
+//        Assert.assertEquals(expected, out, 0.01);
+//    }
 }
