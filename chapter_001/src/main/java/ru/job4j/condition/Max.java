d5da089574f34b659320934fc06a07f50923d79c
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-04-26 13:14:42 +0300

    1.2.1.3. Перегрузить метод max для трех чисел [#271032]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -16,4 +16,31 @@ public class Max {
         int result = left > right ? left : right;
         return result;
     }
+
+    /**
+     * возвращает максимально число из трех
+     * @param first
+     * @param second
+     * @param third
+     * @return
+     */
+    public static int max(int first, int second, int third) {
+        int tmp = max(first, second);
+        int result = third > tmp ? third : tmp;
+        return result;
+    }
+
+    /**
+     * возвращает максимально число из четырех
+     * @param first
+     * @param second
+     * @param third
+     * @param four
+     * @return
+     */
+    public static int max(int first, int second, int third, int four) {
+        int tmp = max(first, second, third);
+        int result = tmp > four ? tmp : four;
+        return result;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -26,4 +26,27 @@ public class MaxTest {
     }
 
 
+    @Test
+    public void maxThird() {
+        int result = Max.max(1, 2, 3);
+        assertThat(result, is(3));
+    }
+
+    /** @noinspection checkstyle:MethodName*/
+    @Test
+    public void maxOneOneThirdThird() {
+        int result = Max.max(1, 1, 3);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void maxFour() {
+        int result = Max.max(1, 2, 3, 4);
+        assertThat(result, is(4));
+    }
+    @Test
+    public void maxOneTwoTwoFourFour() {
+        int result = Max.max(1, 2, 2, 4);
+        assertThat(result, is(4));
+    }
 }
