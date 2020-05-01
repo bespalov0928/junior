ee2dc76eb5f5ba686cdbc7f03a3eab79ed22459e
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-05-01 21:30:40 +0300

    1.2.3.4. Переопределение [#271046] - 1

--- chapter_001/src/main/java/ru/job4j/inheritance/JSONReport.java
+++ chapter_001/src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -4,6 +4,15 @@ public class JSONReport extends TextReport {
 
     @Override
     public String generate(String name, String body) {
-        return "name : " + name + ", body : " + body;
+        String s = "{"
+                + System.lineSeparator()
+                + System.lineSeparator()
+                + "name : " + name + ","
+                + System.lineSeparator()
+                + "body : " + body
+                + System.lineSeparator()
+                + System.lineSeparator()
+                +"}";
+        return s;
     }
 }
