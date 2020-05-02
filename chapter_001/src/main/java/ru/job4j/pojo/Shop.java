44ef881878a23271daf55caa56e11798c2795a65
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-05-02 12:36:29 +0300

    1.2.3.3. Удаление моделей из массива. [#271054] - 1

--- chapter_001/src/main/java/ru/job4j/pojo/Shop.java
+++ chapter_001/src/main/java/ru/job4j/pojo/Shop.java
@@ -53,13 +53,10 @@ public class Shop {
     }
 
     private Product[] delete(Product[] products, int index) {
-        for (; index < products.length; index++) {
-            if (index == products.length - 1) {
-                products[index] = null;
-                break;
-            }
+        for (; index < products.length - 1; index++) {
             products[index] = products[index + 1];
         }
+        products[products.length - 1] = null;
         return products;
     }
 }
