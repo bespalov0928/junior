421c7c1282809f69b7f676af6bce4b61d74f0701
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-05-18 07:58:57 +0300

    1.3.1.1. Телефонный справочник на базе ArrayList [#270973]

--- chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -18,18 +18,8 @@ public class PhoneDictionary {
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getAddress().contains(key)) {
+            if (person.getAddress().contains(key) || person.getName().contains(key) || person.getPhone().contains(key) || person.getSurname().contains(key)) {
                 result.add(person);
-                continue;
-            } else if (person.getName().contains(key)) {
-                result.add(person);
-                continue;
-            } else if (person.getPhone().contains(key)) {
-                result.add(person);
-                continue;
-            } else if (person.getSurname().contains(key)) {
-                result.add(person);
-                continue;
             }
         }
         return result;
