6c300247b11a58dba9220bd2e25fe98157ba63d1
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-05-25 20:50:43 +0300

    1.3.4.3. Компаратор для строк [#270967]

--- chapter_001/src/main/java/ru/job4j/ru/job4j/sort/Machine.java
+++ chapter_001/src/main/java/ru/job4j/ru/job4j/sort/Machine.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Machine {
 
-    private final int[] COINS = {10, 5, 2, 1};
+    static final int[] COINS = {10, 5, 2, 1};
 
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
--- chapter_003/src/main/java/ru/job4j/collection/StringCompare.java
+++ chapter_003/src/main/java/ru/job4j/collection/StringCompare.java
@@ -6,24 +6,17 @@ public class StringCompare implements Comparator<String> {
 
     @Override
     public int compare(String first, String second) {
-        int rsl = 0;
         char[] arrayFirst = first.toCharArray();
         char[] arraySecond = second.toCharArray();
-        int indexMin = arrayFirst.length < arraySecond.length ? arrayFirst.length : arraySecond.length;
-
-            //int maxLength = arrayFirst.length >= arraySecond.length ? arrayFirst.length : arraySecond.length;
+        int rsl = Integer.compare(arrayFirst.length, arraySecond.length);
+            int indexMin = Math.min(arrayFirst.length, arraySecond.length);
             for (int index = 0; index < indexMin; index++) {
-                rsl = Character.compare(arrayFirst[index], arraySecond[index]);
-                if (rsl == 0) {
-                    continue;
+                int rslTemp = Character.compare(arrayFirst[index], arraySecond[index]);
+                if (rslTemp != 0) {
+                    rsl = rslTemp;
+                    break;
                 }
-                break;
             }
-
-
-        if (rsl == 0 && arrayFirst.length != arraySecond.length) {
-            rsl = Integer.compare(arrayFirst.length, arraySecond.length);
-        }
         return rsl;
     }
 }
