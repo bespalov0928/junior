3d6dd76cc8c60d426dd7d9f626b130e7791f4a85
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-06-07 20:32:46 +0300

    1.4.2.1. Фильтрация учеников. [#270840]

--- chapter_004/src/main/java/ru/job4j/streamAPI/School.java
+++ chapter_004/src/main/java/ru/job4j/streamAPI/School.java
@@ -6,11 +6,13 @@ import java.util.stream.Collectors;
 
 public class School {
     public static List<Student> collect(List<Student> students, Predicate<Student> predicate) {
-        List<Student> rsl = students.stream().filter(
-                student -> {
-                    return predicate.test(student);
-                }
-        ).collect(Collectors.toList());
+
+        Predicate<Student> predicat = (student) -> (predicate.test(student));
+
+        List<Student> rsl = students
+                .stream()
+                .filter(predicat)
+                .collect(Collectors.toList());
         return rsl;
     }
 }
