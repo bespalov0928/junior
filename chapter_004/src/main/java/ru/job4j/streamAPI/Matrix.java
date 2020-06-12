24b5cf2c0fc4342717e7ffb8b3c177da57b0ab48
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-06-12 22:16:10 +0300

    1.4.2.5. Преобразование матрицы чисел в список чисел. [#270844]

--- chapter_004/src/main/java/ru/job4j/streamAPI/Matrix.java
+++ chapter_004/src/main/java/ru/job4j/streamAPI/Matrix.java
@@ -1,26 +1,15 @@
 package ru.job4j.streamAPI;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Matrix {
-    public static void main(String[] args) {
-//        List<List<Integer>> matrix = List.of(
-//                List.of(1, 2),
-//                List.of(3, 4)
-//        );
-//
-//        System.out.println(matrix.stream().flatMap(e -> e.stream())
-//                .collect(Collectors.toList()));
 
-        Integer[][] array = new Integer[2][2];
-        array[0][0] = 1;
-        array[0][1] = 2;
-        array[1][0] = 3;
-        array[1][1] = 4;
-        Stream<Integer[]> matrix =  Stream.of(array);
-        System.out.println(matrix.flatMap(e -> Arrays.stream(e))
-                .collect(Collectors.toList()));
+    public static List<Integer> convertInStrem(Integer[][] array) {
+        return Stream.of(array)
+                .flatMap(e -> Arrays.stream(e))
+                .collect(Collectors.toList());
     }
 }
---
+++ chapter_004/src/test/java/ru/job4j/streamAPI/MatrixTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.streamAPI;
+
+
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class MatrixTest {
+
+    @Test
+    public void convertInStrem() {
+        Integer[][] array = new Integer[2][2];
+        array[0][0] = 1;
+        array[0][1] = 2;
+        array[1][0] = 3;
+        array[1][1] = 4;
+
+        List<Integer> result = Matrix.convertInStrem(array);
+        List<Integer> extend = new ArrayList<Integer>();
+        extend.add(1);
+        extend.add(2);
+        extend.add(3);
+        extend.add(4);
+        assertThat(result, is(extend));
+    }
+}
